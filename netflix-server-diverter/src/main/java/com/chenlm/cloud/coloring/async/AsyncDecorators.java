25f7d13cd24e382cde1a0c0715a21576dd9f7e79
黑牛 <chenlingmin.com@gmail.com>
2020-12-29 16:00:01 +0800

    修复同步情况事，未传递流量标识问题

--- netflix-server-diverter/src/main/java/com/chenlm/cloud/coloring/async/AsyncDecorators.java
+++ netflix-server-diverter/src/main/java/com/chenlm/cloud/coloring/async/AsyncDecorators.java
@@ -2,44 +2,21 @@ package com.chenlm.cloud.coloring.async;
 
 import com.chenlm.cloud.ribbon.support.RibbonFilterContextHolder;
 import org.springframework.http.HttpHeaders;
-import org.springframework.web.context.request.RequestAttributes;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
 
-import javax.servlet.http.HttpServletRequest;
-import java.util.Collections;
-import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.Callable;
-import java.util.function.Function;
-import java.util.stream.Collectors;
+
+import static com.chenlm.cloud.coloring.async.HttpHeadersHolder.getHttpHeaders;
 
 /**
  * @author Chenlm
  */
 public final class AsyncDecorators {
 
-    private static HttpHeaders extractHttpHeaders(HttpServletRequest httpRequest) {
-        return Collections.list((Enumeration<String>) httpRequest.getHeaderNames())
-                .stream()
-                .collect(Collectors.toMap(
-                        Function.identity(),
-                        h -> Collections.list((Enumeration<String>) httpRequest.getHeaders(h)),
-                        (oldValue, newValue) -> newValue,
-                        HttpHeaders::new
-                ));
-    }
 
     public static final Decorator<Runnable> HttpHeaderRunnableDecorator = runnable -> {
-        RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();
-        HttpHeaders httpHeaders;
-        if (requestAttributes != null && requestAttributes instanceof ServletRequestAttributes) {
-            httpHeaders = extractHttpHeaders(((ServletRequestAttributes) requestAttributes).getRequest());
-        } else {
-            httpHeaders = HttpHeadersHolder.getHttpHeaders();
-        }
-
+        HttpHeaders httpHeaders = getHttpHeaders();
         if (httpHeaders != null) {
             return () -> {
                 try {
@@ -56,14 +33,7 @@ public final class AsyncDecorators {
 
 
     public static final Decorator<Callable> HttpHeaderCallableDecorator = callable -> {
-        RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();
-        HttpHeaders httpHeaders;
-        if (requestAttributes != null && requestAttributes instanceof ServletRequestAttributes) {
-            httpHeaders = extractHttpHeaders(((ServletRequestAttributes) requestAttributes).getRequest());
-        } else {
-            httpHeaders = HttpHeadersHolder.getHttpHeaders();
-        }
-
+        HttpHeaders httpHeaders = getHttpHeaders();
         if (httpHeaders != null) {
             return () -> {
                 try {
--- netflix-server-diverter/src/main/java/com/chenlm/cloud/coloring/async/HttpHeadersHolder.java
+++ netflix-server-diverter/src/main/java/com/chenlm/cloud/coloring/async/HttpHeadersHolder.java
@@ -2,29 +2,36 @@ package com.chenlm.cloud.coloring.async;
 
 import org.springframework.core.NamedThreadLocal;
 import org.springframework.http.HttpHeaders;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.Collections;
+import java.util.Enumeration;
 import java.util.List;
 import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 /**
  * @author Chenlm
  */
 public final class HttpHeadersHolder {
 
-    private static final ThreadLocal<HttpHeaders> requestAttributesHolder =
+    private static final ThreadLocal<HttpHeaders> httpHeadersThreadLocal =
             new NamedThreadLocal<>("Http Headers");
 
 
     public static void resetHttpHeader() {
-        requestAttributesHolder.remove();
+        httpHeadersThreadLocal.remove();
     }
 
     public static void setHttpHeader(HttpHeaders httpHeaders) {
         if (httpHeaders == null) {
             resetHttpHeader();
         } else {
-            requestAttributesHolder.set(httpHeaders);
+            HttpHeadersHolder.httpHeadersThreadLocal.set(httpHeaders);
         }
     }
 
@@ -35,7 +42,19 @@ public final class HttpHeadersHolder {
      * or {@code null} if none bound
      */
     public static HttpHeaders getHttpHeaders() {
-        return requestAttributesHolder.get();
+        RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();
+        if (requestAttributes != null && requestAttributes instanceof ServletRequestAttributes) {
+            HttpServletRequest httpRequest = ((ServletRequestAttributes) requestAttributes).getRequest();
+            return Collections.list((Enumeration<String>) httpRequest.getHeaderNames())
+                    .stream()
+                    .collect(Collectors.toMap(
+                            Function.identity(),
+                            h -> Collections.list((Enumeration<String>) httpRequest.getHeaders(h)),
+                            (oldValue, newValue) -> newValue,
+                            HttpHeaders::new
+                    ));
+        }
+        return httpHeadersThreadLocal.get();
     }
 
     public static Optional<String> getHeaderValue(String headerName) {
